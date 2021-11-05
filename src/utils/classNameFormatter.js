8dd7464aa12d02b712b14cd21a03516e497c4afc
wichaisw <wichai.sw19@gmail.com>
2021-11-05 22:44:25 +0700

    fix: remove unnecessary util component

--- src/utils/classNameFormatter.js
+++
@@ -1,7 +0,0 @@
-export const classNameFormatter = (input) =>
-    input
-        .replace(/\s+/gm, " ")
-        .split(" ")
-        .filter((cond) => typeof cond === "string")
-        .join(" ")
-        .trim();
